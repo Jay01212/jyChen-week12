ecb58c0d9d8fd921d05290b8266058c657066dbd
Jay01212 <jche0295@student.monash.edu>
2024-10-18 15:53:37 +1100

    Revert to weather1 version while keeping later commit history

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -13,6 +14,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: process.env.NODE_ENV === 'production' ? '/jyChen-library/' : '/'
+  }
 })
